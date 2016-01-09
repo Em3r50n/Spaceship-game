0b6503fbfdc5466cf783cfdaabca6d112ae32bf6
Victor Porto Lambert <victorportolambert@gmail.com>
2016-01-09 00:56:58 -0300

    Script de Resolução e Fix no script de movimentação da nave.

---
+++ Spaceship/Assets/Scripts/Camera/Resolution_Adjust.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resolution_Adjust : MonoBehaviour {
+
+    public float targetRatio = 10f / 16f; //O ratio_aspect no qual
+                                          //o jogo foi desenvolvido.
+    
+    void Start() {
+        Camera cam = GetComponent<Camera>();
+        cam.aspect = targetRatio;
+    } 
+	
+
+	void Update () {
+	
+	}
+}
--- Spaceship/Assets/Scripts/NaveScripts/NaveController.cs
+++ Spaceship/Assets/Scripts/NaveScripts/NaveController.cs
@@ -1,67 +1,107 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NaveController : MonoBehaviour {
+public class NaveController : MonoBehaviour
+{
 
-	//CONSTANTES
-	public const int vel = 10;
-	public const float leftMargin = 0.2F;
-	public const float rightMargin = 0.8F;
+    //CONSTANTES
+    public const int vel = 10;
+    public const float maxDist = 1.40f;
+    //public const float leftMargin = 0.2F;
+    //public const float rightMargin = 0.8F;
 
-	//Atributos da Classe
-	private double halfScreen;
-
-
-
-	void Awake () {
-		halfScreen = Screen.width / 2.0;
-	}
-	void Start () {
-		
-	}
-
-	void LateUpdate () {
-
-		ObjectPosition obj = new ObjectPosition (); 
-		transform.position = obj.getPositionDelimited (leftMargin, rightMargin, transform.position);
-
-		shipMoving ();
+    //Atributos da Classe
+    private double halfScreen;
 
 
 
-		//Para fazer teste no computador é só descomentar esse trecho
-		/*
-		if (Input.GetKey ("left")) {
-			transform.Translate (vel * Time.deltaTime * (-1), 0, 0);
-		}
-		if (Input.GetKey ("right")) {
-			transform.Translate (vel * Time.deltaTime, 0, 0);
-		}
-		*/
+    void Awake()
+    {
+        halfScreen = Screen.width / 2.0;
+    }
+    void Start()
+    {
 
-	}
+    }
+
+    void LateUpdate()
+    {
+        shipMoving();
+
+        //ObjectPosition obj = new ObjectPosition (); 
+        //transform.position = obj.getPositionDelimited (leftMargin, rightMargin, transform.position);
 
 
-	/**
-	 * @return void
-	 * 
-	 * Função responsável por capturar o movimento da nave na tela
-	 * 
-	 * */
-	private void shipMoving () {
+        //Para fazer teste no computador é só descomentar esse trecho
+        /*
+        if (Input.GetKey ("left"))
+        {
 
-		ScreenTouch touchController = new ScreenTouch ();
+            if (transform.position.x > -maxDist)
+            {
+                transform.Translate(-vel * Time.deltaTime, 0, 0);
+            }
+            if (transform.position.x < -maxDist)
+            {
+                transform.position = new Vector3(-1.4f, 0, 0);
+            }
+        }
 
-		int s = touchController.getSideTouch (transform.position, halfScreen);
+        if (Input.GetKey ("right"))
+        {
+            if (transform.position.x < maxDist)
+            {
+                transform.Translate(vel * Time.deltaTime, 0, 0);
+            }
+            if (transform.position.x > maxDist)
+            {
+                transform.position = new Vector3(1.4f, 0, 0);
+            }
+        }
+        */
 
-		if (s == 0) {
-			transform.Translate (Vector3.left * vel * Time.deltaTime);
-		} else if (s == 1) {
-			transform.Translate (Vector3.right * vel * Time.deltaTime);
-		}
+    }
 
-	}
 
+    /**
+     * @return void
+     * 
+     * Função responsável por capturar o movimento da nave na tela
+     * 
+     * */
+    private void shipMoving() {
+
+        ScreenTouch touchController = new ScreenTouch();
+
+        int s = touchController.getSideTouch(transform.position, halfScreen);
+
+        if (s == 0) {
+
+            if (transform.position.x > -maxDist)
+            {
+                transform.Translate(Vector3.left * vel * Time.deltaTime);
+            }
+            if (transform.position.x < -maxDist)
+            {
+                transform.position = new Vector3(-1.4f, 0, 0);
+            }
+        }
+
+        else if (s == 1)
+        {
+            if (transform.position.x < maxDist)
+            {
+                transform.Translate(Vector3.right * vel * Time.deltaTime);
+            }
+            if (transform.position.x > maxDist) {
+                transform.position = new Vector3(1.4f, 0, 0);
+            }
+        }
+    }
 
 }
+
+
+
+
 	
